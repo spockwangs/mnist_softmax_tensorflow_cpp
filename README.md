c3a72d591a55b12e613d219db49a8429d0557cdb
spockwangs <wbbtiger@gmail.com>
2018-01-10 15:13:16 +0800

    Import

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# mnist_softmax_tensorflow_cpp
-Training mnist softmax regression model using TensorFlow C++ API.
+# Introduction
+
+Train mnist softmax regression model using TensorFlow C++ API.
+
+# Requirements
+
+- TensorFlow C++ API 1.5
+- Python 2.x
+- [MNIST data, 4 files](http://yann.lecun.com/exdb/mnist/)
+
+# How to use
+
+Export the graph:
+
+```
+$ create_mnist_softmax_model.py --export_dir models
+```
+
+Compile the training code:
+```
+$ g++ train_mnist_softmax_model.cpp -o train_mnist_softmax_model -ltensorflow_cc -ltensorflow_framework -std=c++11 -ldl
+```
+
+Download the MNIST data to a directory and unzip.
+
+Run the training:
+```
+$ train_mnist_softmax_model <data_dir> <graph file>
+```
+
---
+++ create_mnist_softmax_model.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python2
+#-*- coding: utf-8 -*-
+#
+# @author wbbtiger@gmail.com
+# @brief Create a softmax regression model for mnist, and export as binary protobuf.
+#
+
+import tensorflow as tf
+import getopt
+import sys
+import exception
+import traceback
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def usage(progname):
+    print('''Usage:
+    {progname} [-h | --help]
+    {progname} --export_dir <dir>'''.format(progname=progname))
+    
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "h", [ 'help', 'export_dir=', 'as_text' ])
+        except getopt.error as msg:
+            raise Usage(msg)
+
+        global g_debug_mode
+        export_dir = ''
+        as_text = False
+        for o, a in opts:
+            if o in ('-h', '--help'):
+                usage(argv[0])
+                return 0
+            elif o in ('-d'):
+                g_debug_mode = True
+            elif o in ('--export_dir'):
+                export_dir = a
+            elif o in ('--as_text'):
+                as_text = True
+            else:
+                raise Usage('Bad option: %s' % (o))
+        if len(export_dir) == 0:
+            raise Usage('Bad value for option --export_dir')
+
+        # Create the model
+        x = tf.placeholder(tf.float32, [None, 784], name='x')
+        W = tf.Variable(tf.zeros([784, 10]))
+        b = tf.Variable(tf.zeros([10]))
+        y = tf.matmul(x, W) + b
+
+        # Define loss and optimizer
+        y_ = tf.placeholder(tf.int32, [None], name='y_')
+        cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=y_, logits=y)
+        train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy, name='train_step')
+
+        # Test trained model
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.cast(y_, tf.int64))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
+
+        # Export the computation graph.
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            tf.train.write_graph(sess.graph_def, export_dir, 'mnist_graph.pb', as_text=as_text)
+    except Usage as err:
+        print(err.msg)
+        usage(argv[0])
+        return 1
+    except Exception as e:
+        traceback.print_exc()
+        return 1
+
+if __name__ == '__main__':
+    main()
